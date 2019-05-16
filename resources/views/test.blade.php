73adf3b15463e7ae136ba3e0c0046d4b157da0dd
alibek009 <37546387+alibek009@users.noreply.github.com>
2019-05-16 20:27:57 +0600

    Quiz bug fixed again last time

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -56,19 +56,24 @@
                             <div class="alert alert-info">Your test score is 100 % </div>
                             <br>
                             @endif
-
-
                         @foreach($lesson->test->questions as $question)
 
                             <b>{{ $loop->iteration }}. {{$question->question}}</b>
-                            <img src="{{ $question->question_image }}" alt="">
                             <br>
                             @foreach($question->options as $option)
+                                @if($option->correct == 1)
                                 <input type="radio" name="questions[{{ $question->id }}]" value="{{ $option->id }}" />
+
                                     <p style="background-color: green; "><b>{{ $option->option_text }}</b></p>
+                                    @else
+                                    <input type="radio" name="questions[{{ $question->id }}]" value="{{ $option->id }}" />
+
+                                    <b>{{ $option->option_text }}</b>
+                                    <br>
+                                @endif
 
 
-                            @endforeach
+                                    @endforeach
 
 
                         @endforeach
